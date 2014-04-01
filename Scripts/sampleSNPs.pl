52f29ee4e413aa6800698c6db8f95c98f73e807e
Jeremy J Berg <jeremy.jackson.berg@gmail.com>
2014-04-01 15:26:50 -0700

    messing with projection matrix stuff

---
+++ Scripts/sampleSNPs.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+use strict;
+
+
+# Usage $ program.pl <uniq_snps> <data_file>
+# uniq_snps:
+# rs0001
+# rs0002
+# rs0006
+# rs0001
+# ...
+
+
+my $uniq_filename = shift(@ARGV);
+open(my $fh, "<$uniq_filename");
+
+my %snps;
+while(<$fh>)
+{
+    chomp;
+    my $line = $_;
+    # $line =~ s/\[\[\:space\:\]\]//;
+    $snps{$line} += 1;
+}
+close($fh);
+
+while(<>)
+{
+    # rs0123   USA  A   C   1\n
+    chomp;
+    # rs0123   USA  A   C   1
+    my $line = $_;
+    # rs0123   USA  A   C   1
+    my @split_line = split(/\s+/, $line);
+    # array = ["rs0123", "USA", "A", "C", "1"]
+    if (defined($snps{$split_line[0]}))
+    {
+        print "$line\n";
+    }
+}
+
